0d9196e349f09c1192a5b516b749874e8f16f8d3
dominicbraam <dominic.braam@gmail.com>
2022-04-13 22:44:30 -0400

    bot can grab calendar events and reply to users with the compiled results

---
+++ bot.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from dateutil.parser import parse as dtparse
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+from os import getenv
+import cal_handler
+load_dotenv()
+
+def main():
+    bot = commands.Bot(command_prefix='!')
+
+    intents = discord.Intents.default()
+    intents.members = True
+
+    # time_format = '%d %B, %H:%M %p'
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.reply('Hey there!')
+
+    @bot.command()
+    async def today(ctx):
+        reply = create_summary(date.today())
+        await ctx.reply(reply)
+
+    @bot.command()
+    async def tomorrow(ctx):
+        reply = create_summary(date.today() + timedelta(days=1))
+        await ctx.reply(reply)
+
+    bot.run(getenv('TOKEN'))
+
+def compile_classes(classes):
+    time_format = '%H:%M'
+
+    if not classes:
+        classes_summary = "You have no classes!!!"
+    else:
+        classes_summary = ""
+        for class_ in classes:
+            event_time = datetime.strftime(dtparse(class_["start"]["dateTime"]), format=time_format)
+            event_item = class_["summary"] + " - " + event_time
+            classes_summary += event_item + "\n"
+
+    compiled_classes = "-- CLASSES --\n" + classes_summary
+
+    return compiled_classes
+
+def create_summary(date):
+    events = cal_handler.get_events(date)
+    summary = compile_classes(events)
+    return summary
+
+if __name__ == "__main__":
+    main()
---
+++ cal_handler.py
@@ -0,0 +1,31 @@
+# Google Cal Handler
+# Help:
+# - https://karenapp.io/articles/how-to-automate-google-calendar-with-python-using-the-calendar-api/
+# - https://qxf2.com/blog/google-calendar-python/
+
+from cal_init import get_calendar_service
+from dateutil.parser import parse as dtparse
+from datetime import datetime
+from os import getenv
+
+def get_events(date):
+
+    year = int(datetime.strftime(date, format='%Y'))
+    month = int(datetime.strftime(date, format='%m'))
+    day = int(datetime.strftime(date, format='%d'))
+    start_date = datetime(year, month, day, 0, 0, 0, 0).isoformat() + 'Z'
+    end_date = datetime(year, month, day, 23, 59, 59, 0).isoformat() + 'Z'
+
+    service = get_calendar_service()
+
+    # calendar_id = "dominic.braam@gmail.com"
+    calendar_id = getenv('CAL_ID')
+    print('\n----%s:\n' % calendar_id)
+    eventsResult = service.events().list(
+        calendarId=calendar_id,
+        timeMin=start_date,
+        timeMax=end_date,
+        singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+    return events
